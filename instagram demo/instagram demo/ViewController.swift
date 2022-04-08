c7fe12df1adf70e29bd6dd9b5ec6dcdeda1ae809
Abylay <aurelioleclarke@gmail.com>
2022-04-08 21:55:57 +0600

    Instagram 1.1

--- instagram demo/instagram demo/ViewController.swift
+++ instagram demo/instagram demo/ViewController.swift
@@ -14,22 +14,33 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     @IBOutlet weak var MainTableView: UITableView!
     
     var models = [InstagramPost]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         MainTableView.register(TableViewCell.nib(), forCellReuseIdentifier: TableViewCell.identifier)
         MainTableView.delegate = self
         MainTableView.dataSource = self
+        
+        models.append(InstagramPost(numberOfLikes: 254, userName: "@Aureliola", userImageName: "head", mainImageName: "post_1"))
+        models.append(InstagramPost(numberOfLikes: 301, userName: "@LeoMessi", userImageName: "head", mainImageName: "post_2"))
+        
+        models.append(InstagramPost(numberOfLikes: 280, userName: "@JLO", userImageName: "head", mainImageName: "post_3"))
+        
     }
 
-
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120+140+view.frame.size.width
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
+  
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier, for: indexPath) as! TableViewCell
         cell.configure(with: models[indexPath.row])
-        return UITableViewCell()
+        return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
